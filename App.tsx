e585a6ecb69423787d0d96aa07053e4f202b61c8
Joseph gachiri <joseph.gachiri1@gmail.com>
2023-11-03 17:48:42 +0300

    updated the app icon and splash screen

--- App.tsx
+++ App.tsx
@@ -1,13 +1,15 @@
 import "react-native-gesture-handler";
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
-import React from 'react';
+import React,{useEffect} from 'react';
 import Routes from './navigation/routes';
 import {Provider} from 'react-redux';
 import store from './redux/store/store';
 import FlashMessage from 'react-native-flash-message';
-
+import SplashScreen from "react-native-splash-screen";
 export default function App() {
-
+    useEffect(()=>{
+      SplashScreen.hide()
+    },[])
 
 
   // please check developer mode in navigation.js before continuing.
